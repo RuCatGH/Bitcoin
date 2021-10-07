4fc814129b19fae928e4d5a791edd73866f8fbf2
RuCat <cvlromacvl@gmail.com>
2021-10-07 12:58:26 +0300

    deletepricebitcoin

--- bot.py
+++ bot.py
@@ -1,18 +1,34 @@
 import logging
+from typing import Text
 from aiogram import Bot, Dispatcher, executor, types
 import config
+import requests
+import json
+from aiogram.dispatcher.filters import Text
+
+# Делаем запрос на получение цены биткоина
+response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd').text
+bitcoinjson = json.loads(response)
+pricebitcoin = bitcoinjson["bitcoin"]["usd"]
+
 # Объект бота
 bot = Bot(config.token)
+
 # Диспетчер для бота
 dp = Dispatcher(bot)
+
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
+# Хэндлер на команду /price
+@dp.message_handler(commands="price")
+async def priceb(message: types.Message):
+    await message.answer(f"Цена биткоина = {pricebitcoin}")
 
-# Хэндлер на команду /test1
-@dp.message_handler(commands="test1")
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+@dp.message_handler(Text(equals = 'будильник', ignore_case = True))
+async def alarm(message: types.Message):
+    await message.answer(message.text)
+    
 
 
 if __name__ == "__main__":
